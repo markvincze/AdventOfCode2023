25906314bc3fb94e2d74df13c49399a69e4f175e
Mark Vincze <mrk.vincze@gmail.com>
2023-12-04 21:30:34 +0100

    Day 3

--- FSharp/03-gear-ratios.fsx
+++ FSharp/03-gear-ratios.fsx
@@ -25,22 +25,61 @@ let hasAdjacentSymbol x y schematic =
     isSymbol (x - 1) (y + 1) schematic ||
     isSymbol (x - 1) (y) schematic
 
-let rec readSymbol x y (schematic: char[,]) digits hasAnyAdjacentSymbol =
+let getAdjacentSymbols x y schematic =
+    [ if isSymbol (x - 1) (y - 1) schematic then yield (x - 1), (y - 1)
+      if isSymbol (x) (y - 1) schematic then yield (x), (y - 1)
+      if isSymbol (x + 1) (y - 1) schematic then yield (x + 1), (y - 1)
+      if isSymbol (x + 1) (y) schematic then yield (x + 1), (y)
+      if isSymbol (x + 1) (y + 1) schematic then yield (x + 1), (y + 1)
+      if isSymbol (x) (y + 1) schematic then yield (x), (y + 1)
+      if isSymbol (x - 1) (y + 1) schematic then yield (x - 1), (y + 1)
+      if isSymbol (x - 1) (y) schematic then  yield (x - 1), (y) ] |> Set.ofList
+
+let rec readSymbol x y (schematic: char[,]) digits adjacentSymbols =
     if x >= Array2D.length1 schematic || Char.IsDigit (schematic.[x, y]) |> not
-    then if hasAnyAdjacentSymbol
-         then digits |> List.mapi (fun i d -> pown 10 i * d) |> List.sum |> Some
+    then if Set.isEmpty adjacentSymbols |> not
+         then (digits |> List.mapi (fun i d -> pown 10 i * d) |> List.sum, adjacentSymbols) |> Some
          else None
-    else readSymbol (x + 1) y schematic ((schematic.[x, y] |> string |> Int32.Parse) :: digits) (hasAnyAdjacentSymbol || hasAdjacentSymbol x y schematic)
+    else let adjacentSymbols = getAdjacentSymbols x y schematic |> Set.union adjacentSymbols
+         readSymbol (x + 1) y schematic ((schematic.[x, y] |> string |> Int32.Parse) :: digits) adjacentSymbols
 
 let getPartNumber x y (schematic: char[,]) =
     let c = schematic.[x, y]
 
     if Char.IsDigit c |> not then None
     else if x > 0 && Char.IsDigit (schematic.[x - 1, y]) then None
-    else readSymbol x y schematic [] false
+    else readSymbol x y schematic [] Set.empty
 
 let result1 =
     [ for x in 0..(Array2D.length1 schematic - 1) do
           for y in 0..(Array2D.length2 schematic - 1) -> (x, y)]
     |> List.choose (fun (x, y) -> getPartNumber x y schematic)
-    |> List.sum
+    |> List.sumBy fst
+
+let partNumbers = 
+    [ for x in 0..(Array2D.length1 schematic - 1) do
+          for y in 0..(Array2D.length2 schematic - 1) -> (x, y)]
+    |> List.choose (fun (x, y) -> getPartNumber x y schematic)
+
+let gears =
+    List.fold
+        (fun gears partNumber ->
+            Set.fold
+                (fun gs (x, y) ->
+                    if schematic.[x, y] = '*'
+                    then gs
+                         |> Map.change
+                                (x, y)
+                                (fun ps -> match ps with
+                                           | None -> [ fst partNumber ] |> Some
+                                           | Some ps -> fst partNumber :: ps |> Some)
+                    else gs)
+                gears
+                (snd partNumber))
+        Map.empty<int * int, int list> 
+        partNumbers
+
+let result2 = gears
+              |> Map.filter (fun _ ps -> List.length ps = 2)
+              |> Map.values
+              |> Seq.sumBy (fun ps -> ps.[0] * ps.[1])
