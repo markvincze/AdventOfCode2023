dcbb5da5fb28c6165077e75184760b7dbf3f1f3b
Mark Vincze <mvincze@travix.com>
2023-12-07 16:59:31 +0100

    Day 7 part 1

--- FSharp/07-camel-cards.fsx
+++ FSharp/07-camel-cards.fsx
@@ -1,3 +1,6 @@
+open System
+open System.IO
+
 type HandType =
     | FiveOfAKind of int
     | FourOfAKind of int
@@ -25,24 +28,65 @@ let getHandType (hand: Hand) =
     else if groups |> Array.length = 2
     then if groups.[0] |> Array.length = 4
          then FourOfAKind (groups.[0].[0]), [ groups.[1][0] ]
-         else FullHouse (groups.[0].[0], groups.[1][0]), []
+         else FullHouse (if groups.[0].[0] > groups.[1].[0] then (groups.[0].[0], groups.[1][0]) else (groups.[1].[0], groups.[0][0])), []
     else if groups |> Array.length = 3
     then if groups.[0] |> Array.length = 3
          then ThreeOfAKind (groups.[0].[0]), [ groups.[1].[0]; groups.[2].[0] ]
-         else TwoPair (groups.[0].[0], groups.[1][0]), [ groups.[2].[0] ]
+         else TwoPair (if groups.[0].[0] > groups.[1].[0] then (groups.[0].[0], groups.[1][0]) else (groups.[1].[0], groups.[0][0])), [ groups.[2].[0] ]
     else if groups |> Array.length = 4
     then OnePair groups.[0].[0], [ groups.[1].[0]; groups.[2].[0]; groups.[3].[0] ]
     else HighCard groups.[0].[0], [ groups.[1].[0]; groups.[2].[0]; groups.[3].[0]; groups.[4].[0] ]
 
-let compare h1 h2 =
-    let ht1, cs1 = getHandType h1
-    let ht2, cs2 = getHandType h2
+let comparePairwise cs1 cs2 =
+    match Seq.zip cs1 cs2 |> Seq.tryFind (fun (x, y) -> x <> y) with
+    | Some (x, y) -> compare x y
+    | None -> failwith "There should be no draws"
+
+let compareHands h1 h2 =
+    let ht1, _ = getHandType h1
+    let ht2, _ = getHandType h2
 
     if getHandTypeScore ht1 < getHandTypeScore ht2 then -1
-    else if getHandTypeScore ht1 > getHandTypeScore ht2 then -1
-    else match ht1, ht2 with
-         | FiveOfAKind x1, FiveOfAKind x2 -> compare x1 x2
-         | FourOfAKind x1, FourOfAKind x2 when x1 <> x2 -> compare x1 x2
-         | FourOfAKind x1, FourOfAKind x2 -> compare x1 x2
+    else if getHandTypeScore ht1 > getHandTypeScore ht2 then 1
+    else comparePairwise h1 h2
+        
+        // Oops
+        //  | FiveOfAKind x1, FiveOfAKind x2 -> compare x1 x2
+        //  | FourOfAKind x1, FourOfAKind x2 when x1 <> x2 -> compare x1 x2
+        //  | FourOfAKind _, FourOfAKind _ -> compareRemaining cs1 cs2
+        //  | FullHouse (x1, _), FullHouse (x2, _) when x1 <> x2 -> compare x1 x2
+        //  | FullHouse (_, y1), FullHouse (_, y2) when y1 <> y2 -> compare y1 y2
+        //  | FullHouse _, FullHouse _ -> compareRemaining cs1 cs2
+        //  | ThreeOfAKind x1, ThreeOfAKind x2 when x1 <> x2 -> compare x1 x2
+        //  | ThreeOfAKind _, ThreeOfAKind _ -> compareRemaining cs1 cs2
+        //  | TwoPair (x1, _), TwoPair (x2, _) when x1 <> x2 -> compare x1 x2
+        //  | TwoPair (_, y1), TwoPair (_, y2) when y1 <> y2 -> compare y1 y2
+        //  | TwoPair _, TwoPair _ -> compareRemaining cs1 cs2
+        //  | OnePair x1, OnePair x2 when x1 <> x2 -> compare x1 x2
+        //  | OnePair _, OnePair _ -> compareRemaining cs1 cs2
+        //  | HighCard x1, HighCard x2 when x1 <> x2 -> compare x1 x2
+        //  | HighCard _, HighCard _ -> compareRemaining cs1 cs2
+        //  | _ -> failwith "Some case was not handled."
 
+let parseLine (line: string) =
+    let [| hand; bid |] = line.Split ' '
+    hand |> Seq.map (fun c -> match c with
+                              | 'T' -> 10
+                              | 'J' -> 11
+                              | 'Q' -> 12
+                              | 'K' -> 13
+                              | 'A' -> 14
+                              | c -> (int c) - 48)
+         |> Array.ofSeq, (Int64.Parse bid)
 
+let hands = File.ReadAllLines "FSharp/07-camel-cards-input.txt"
+            |> Array.map parseLine
+            |> List.ofArray
+            // |> List.sortWith compareHands
+            // |> List.map getHandType
+
+let result1 = hands
+              |> List.sortWith (fun (h1, _) (h2, _) -> compareHands h1 h2)
+              |> List.map snd
+              |> List.indexed
+              |> List.sumBy (fun (rank, bid) -> (rank + 1 |> int64) * bid)
