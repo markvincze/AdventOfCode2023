3d7e5109e617907f5bf5703c2293b03d82e78a27
Mark Vincze <mvincze@travix.com>
2023-12-08 18:37:49 +0100

    Day 8 part 2

--- FSharp/08-haunted-wasteland.fsx
+++ FSharp/08-haunted-wasteland.fsx
@@ -1,4 +1,3 @@
-open System
 open System.IO
 
 type Element = {
@@ -12,11 +11,6 @@ let parseElement (line: string) =
     let [| left; right |] = rest.Split(", ")
 
     name, (left, right.Substring(0, right.IndexOf ')'))
-    // {
-    //     name = name
-    //     left = left
-    //     right = right.Substring(0, right.IndexOf ')')
-    // }
 
 let parse lines =
     let steps = List.head lines
@@ -38,3 +32,50 @@ let rec stepUntilEnd (steps: string) elements pos count =
          stepUntilEnd steps elements (step s pos elements) (count + 1)
 
 let result1 = stepUntilEnd steps elements "AAA" 0
+
+let stepAll s (ps: string array) elements =
+    for i in 0..(Array.length ps - 1) do
+        ps.[i] <- step s ps.[i] elements
+
+let rec stepUntilEnd3 (steps: string) elements (ps: string array) count =
+    if ps |> Array.forall (fun p -> p.EndsWith("Z"))
+    then count
+    else let s = steps.[count % steps.Length]
+         stepAll s ps elements
+         stepUntilEnd3 steps elements ps (count + 1)
+
+// Brute force approach, too slow.
+// let result3 = stepUntilEnd3 steps elements (elements |> Map.keys |> Seq.filter (fun p -> p.EndsWith("A")) |> Array.ofSeq) 0
+
+let rec findCycle (steps: string) elements (pos: string) count firstHit =
+    match pos.EndsWith "Z", firstHit with
+    | true, None ->
+        let s = steps.[count % steps.Length]
+        findCycle steps elements (step s pos elements) (count + 1) (Some (pos, count))
+    | true, Some (p, c) -> (p, c), (pos, count)
+    | false, _ ->
+        let s = steps.[count % steps.Length]
+        findCycle steps elements (step s pos elements) (count + 1) firstHit
+
+let cycles =
+    elements
+    |> Map.keys 
+    |> Seq.filter (fun p -> p.EndsWith("A"))
+    |> List.ofSeq
+    |> List.map (fun p -> p, (findCycle steps elements p 0 None))
+
+let cycleNumbers = cycles |> List.map (snd >> fst >> snd) |> List.map int64
+
+// Least common multiple implementation
+let rec gcd a b =
+    if b = 0L then a
+    else gcd b (a % b)
+
+let lcm a b =
+    if a = 0L && b = 0L then 0L
+    else abs (a * b) / gcd a b
+
+let lcmOfList numbers =
+    List.fold lcm 1L numbers
+
+let result2 = lcmOfList cycleNumbers
